cd28b9f455e9837f22a8cd4f24b93cff3588bb40
lipika1911 <lipikaarya1723@gmail.com>
2025-09-28 19:42:08 +0530

    initial setup done using vite and shadCN

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div>
+      Sky Cast ☀️
+      <Button variant="destructive">Check Weather</Button>
+    </div>
+  )
+}
+
+export default App
